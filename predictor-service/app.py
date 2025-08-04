246f11d66536f04fb05d739f1ef2044c9eb766cd
vibhupuri <vibhupuri@users.noreply.github.com>
2025-08-04 21:37:36 +0530

    Update app.py

--- predictor-service/app.py
+++ predictor-service/app.py
@@ -2,10 +2,23 @@ from fastapi import FastAPI, Request
 import joblib
 import numpy as np
 import os
+import requests
 
 app = FastAPI()
-model_path = os.getenv("MODEL_PATH", "/shared/model.pkl")
-model = joblib.load(model_path)
+
+# GitHub release URL for model.pkl
+MODEL_URL = "https://github.com/vibhupuri/credit-card-fraud-detection-mlops/releases/download/v1.0/model.pkl"
+MODEL_LOCAL_PATH = "/tmp/model.pkl"
+
+# Download model if not already present
+if not os.path.exists(MODEL_LOCAL_PATH):
+    print("Downloading model...")
+    response = requests.get(MODEL_URL)
+    with open(MODEL_LOCAL_PATH, "wb") as f:
+        f.write(response.content)
+
+# Load model
+model = joblib.load(MODEL_LOCAL_PATH)
 
 @app.post("/predict")
 async def predict(request: Request):
